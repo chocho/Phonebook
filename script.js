952ad5cfeaeb0c1d4efad28228462a25a2363d9c
Chocho Acer-Windows <chochosf@gmail.com>
2016-04-23 15:00:55 +0300

    first

---
+++ script.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    $("#dialog").dialog({autoOpen: false});
+
+$("#addRec").click(function () {
+    $("#dialog").dialog("open");
+});
+$("#dialog").dialog({
+    autoOpen: false,
+    height: 500,
+    width: 350,
+    modal: true,
+    buttons: [
+        {
+            text: "Create Record",
+            icons: {
+                primary: "ui-icon-heart"
+            },
+            click: addRecord
+        },
+        {
+            text: "Cancel",
+            icons: {
+                primary: "ui-icon-heart"
+            },
+            click: function () {
+                $(this).dialog("close");
+            }
+        }
+    ]
+});
+
+    function addRecord() {
+
+        $( "#phoneTable tbody" ).append( "<tr>" +
+          "<td>" + $( "#phone" ).val() + "</td>" +
+          "<td>" + $( "#name" ).val() + "</td>" +
+          "<td>" + $( "#place" ).val() + "</td>" +
+          "<td>" + $( "#gender" ).val() + "</td>" +
+          "<td>" + $( "#zodiac" ).val() + "</td>" +
+        "</tr>" );
+        $(this).dialog( "close" );
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+function clickCounter() {
+    if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    } else {
+        localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You have clicked the button " +
+            localStorage.clickcount + " time(s).";
+}
+
+});
