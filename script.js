13c08052f320f47779c01f9687d605c1d266b619
Chocho <Chocho@Chocho-PC>
2016-04-26 17:05:34 +0300

    napredvame

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 $(document).ready(function () {
-           localStorage.users = {};
-
     function addRecord() {
-        var test = + $("#phone").val();
-            localStorage.users = {};
-
-             localStorage.phone = $("#phone").val();
-        //localStorage.users[test]['name'] = $("#name").val();
-        //localStorage.users[test]['place'] = $("#place").val();
-        //localStorage.users[test]['gender'] = $("#gender").val();
-        //localStorage.users[test]['zodiac'] = $("#zodiac").val();
-        //localStorage.users[test]['note'] = $("#note").val();
-        var proba = 555;   
-        
-        
-        $("#phoneTable tbody").append("<tr class='row' id='"+ test +"'>" +
+        var thisUser = {};
+        var test = thisUser.phone = $("#phone").val();
+        thisUser.name = $("#name").val();
+        thisUser.place = $("#place").val();
+        thisUser.gender = $("#gender").val();
+        thisUser.zodiac = $("#zodiac").val();
+        thisUser.note = $("#note").val();
+        var potrebiteli = JSON.parse(localStorage.getItem("users"));
+        if (potrebiteli) {
+            potrebiteli[test] = thisUser;
+            localStorage.setItem("users", JSON.stringify(potrebiteli));
+        } else {
+            var potrebiteli = {};
+            potrebiteli[test] = thisUser;
+            localStorage.setItem("users", JSON.stringify(potrebiteli));
+        }
+        $("#phoneTable tbody").append("<tr class='row' id='" + test + "'>" +
                 "<td>" + $("#phone").val() + "</td>" +
                 "<td>" + $("#name").val() + "</td>" +
                 "<td>" + $("#place").val() + "</td>" +
@@ -27,29 +29,50 @@ $(document).ready(function () {
         $("img").click(function () {
             $("#" + test + "").remove();
             ;
-        });        
-    };
+        });
+    }
+    ;
 
-    function listRecords(){
-      if (localStorage.users.length > 0) {
-          for(c in localStorage.users){
-                $("#phoneTable tbody").append("<tr class='row' id='"+ localStorage.users[c][phone] +"'>" +
-                "<td>" + localStorage.users[c][phone] + "</td>" +
-                "<td>" + localStorage.users[c][name] + "</td>" +
-                "<td>" + localStorage.users[c][place] + "</td>" +
-                "<td>" + localStorage.users[c][gender] + "</td>" +
-                "<td>" + localStorage.users[c][zodiac] + "</td>" +
-                "<td><img  src='images/trash.png' width='25' height='25' alt='' /></td>" +
-                "</tr>");
-          }
-    };  
-    };
-    
+    function listRecords() {
+        var restoredUsers = JSON.parse(localStorage.getItem("users"));
+        if (restoredUsers) {
+            var userrr = {};
+            for (userrr in restoredUsers) {
+                var telefon = userrr[phone];
+                $("#phoneTable tbody").append("<tr class='row' id='" + restoredUsers[userrr].phone + "'>" +
+                        "<td>" + restoredUsers[userrr].phone + "</td>" +
+                        "<td>" + restoredUsers[userrr].name + "</td>" +
+                        "<td>" + restoredUsers[userrr].place + "</td>" +
+                        "<td>" + restoredUsers[userrr].gender + "</td>" +
+                        "<td>" + restoredUsers[userrr].zodiac + "</td>" +
+                        "<td><img onclick='getElementById(\"demo\").innerHTML=Date()' class='"   + restoredUsers[userrr].phone +  "' src='images/trash.png' width='25' height='25' alt='' /></td>" +
+                        "</tr>");
+               $("img").on("click", function(){
+    $(this).parentsUntil("tbody").hide();
+});
+            }
+        }
+        ;
+    }
+    ;
+
+    function removeRec(id) {
+        $("#" + id + "").remove();
+        var restoredUsers = JSON.parse(localStorage.getItem("users"));
+        localStorage.removeItem(restoredUsers[id]);
+        localStorage.setItem("users", JSON.stringify(restoredUsers));
+    }
+
     $("#dialog").dialog({autoOpen: false});
 
     $("#addRec").click(function () {
         $("#dialog").dialog("open");
     });
+
+    $("#clearLS").click(function () {
+        localStorage.clear();
+    });
+
     $("#dialog").dialog({
         autoOpen: false,
         height: 500,
@@ -74,5 +97,5 @@ $(document).ready(function () {
             }
         ]
     });
-//listRecords();
+    listRecords();
 });
